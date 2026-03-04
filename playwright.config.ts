4bdd9fcbb67cbe5a3374479c5892964ad9b1f963
Aaron Ellington <aaron@ellington.io>
2026-03-03 19:52:05 -0500

    make tests run faster

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "@playwright/test"
 
 export default defineConfig({
 	testDir: "./src/tests",
+	fullyParallel: true,
 	globalSetup: "./src/tests/global-setup.ts",
 	outputDir: "./.config/tmp/tests/test-results",
 	use: {
